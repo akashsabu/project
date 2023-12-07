e7be028b46b731d2602c4aaab4cb203c2a132a98
akashsabu <akashpaulmsabu1999@gmail.com>
2023-12-08 00:21:46 +0530

    product model has been created

--- src/products/web/urls.py
+++ src/products/web/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from web.views import index
 
 app_name="web"
@@ -6,3 +9,4 @@ app_name="web"
 urlpatterns = [
     path("", index, name="index")
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
