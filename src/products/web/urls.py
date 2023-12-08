72020ac778c4d20399d934e0d36400ebe9142fe0
akashsabu <akashpaulmsabu1999@gmail.com>
2023-12-08 11:04:02 +0530

    product_detail html created

--- src/products/web/urls.py
+++ src/products/web/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from web.views import index
+from web.views import index, product_detail
 
 app_name="web"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+   
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
